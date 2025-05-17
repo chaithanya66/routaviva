f6bc37155ef1b1b805a85e51c26e10bd86fe6bf0
chaithanya66 <neelamchaithanya6@gmail.com>
2025-05-17 21:39:27 +0530

    dashboard changes

--- constants/constants.js
+++ constants/constants.js
@@ -2,3 +2,4 @@ const base_url="https://rutavivaunsecured-1.onrender.com";
 export const register_url=base_url+"/register";
 export const login_url=base_url+"/login";
 export const homepage_url=base_url+"/homePage/";
+export const trip_creation_url=base_url+"/trip";
